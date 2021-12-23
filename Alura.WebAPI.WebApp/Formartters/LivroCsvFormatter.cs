599909e35c61a29a1d52dfcb6e76efaa8826ed35
Igor Tudisco <igortudisco@hotmail.com>
2021-12-22 21:06:54 -0300

    Add formatação própria no retorno da API.

---
+++ Alura.WebAPI.WebApp/Formartters/LivroCsvFormatter.cs
@@ -0,0 +1,48 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.Formartters
+{
+    // Erdando da class abstrata que indica que é um formato do tipo text que vai sair.
+    public class LivroCsvFormatter : TextOutputFormatter
+    {
+        public LivroCsvFormatter()
+        {
+            var textCsvMediaType = MediaTypeHeaderValue.Parse("text/csv");
+            var appCsvMediaType = MediaTypeHeaderValue.Parse("application/csv");
+            SupportedMediaTypes.Add(textCsvMediaType);
+            SupportedMediaTypes.Add(appCsvMediaType);
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        // Método da class implementado.
+        // vai ser subescrito por esse.
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            // Criando a variavel que vai ter o meu formado de escrita.
+            var livroEmCsv = "";
+
+            // Verificando o obj
+            if (context.Object is LivroApi)
+            {
+                // Passando o obj como um livroApi
+                var livro = context.Object as LivroApi;
+
+                // Pegando o obj e colocando no formado que eu quero.
+                livroEmCsv = $"{livro.Titulo};{livro.Subtitulo};{livro.Autor};{livro.Lista}";
+
+            }
+
+            // Usando o escritor para passar na minha requisição.
+            using (var escritor = context.WriterFactory(context.HttpContext.Response.Body, selectedEncoding))
+            {
+                return escritor.WriteAsync(livroEmCsv);
+            }// escritor.Close()
+
+        }
+    }
+}
